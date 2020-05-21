aa0b53fca7e09ece1c9fcf1afd04983f33610d77
Kreggy <filip.makrlik@gmail.com>
2020-05-22 00:41:46 +0200

    indent

--- app/Presenters/UserPresenenter.php
+++ app/Presenters/UserPresenenter.php
@@ -31,24 +31,24 @@ final class UserPresenter extends Nette\Application\UI\Presenter
 		
 	}
 	
-    protected function createComponentLoginForm(): UI\Form
-    {
-        $form = new UI\Form;
-        $form->addText('name', 'Jmeno:');
-        $form->addPassword('password', 'Heslo:');
-        $form->addSubmit('login', 'Login');
-        $form->onSuccess[] = [$this, 'loginFormSucceeded'];
-        return $form;
-    }
+	protected function createComponentLoginForm(): UI\Form
+	{
+		$form = new UI\Form;
+		$form->addText('name', 'Jmeno:');
+		$form->addPassword('password', 'Heslo:');
+		$form->addSubmit('login', 'Login');
+		$form->onSuccess[] = [$this, 'loginFormSucceeded'];
+		return $form;
+	}
 
-    public function loginFormSucceeded(UI\Form $form, \stdClass $values): void
-    {
+	public function loginFormSucceeded(UI\Form $form, \stdClass $values): void
+	{
 		Debugger::barDump($values);
 		
-        $this->flashMessage('Byl jste uspesne prihlasen.');
+		$this->flashMessage('Byl jste uspesne prihlasen.');
 		$this->redirect('Text:');
 		$values->name;
 		
 		
-    }
+	}
 }
